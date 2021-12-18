c939838e8786bb6433b5e5c92a10639b6ab4213b
Prakul Sharma <prakul.1307@gmail.com>
2021-12-19 03:50:07 +0530

    Flask files for the website

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import itertools
-
 import bs4
 import requests
-
 from utils import *
 
 
 def get_interactions_from_med(med_names: str, print_output: bool = False):
     med_names = med_names.split(",")
     med_names = [med_name.strip() for med_name in med_names]
-    drug_dict = get_drug_name_from_med(med_names)
-    get_interactions_from_drug(drug_dict)
+
+    if len(med_names) == 1:
+        return "Enter more than 1 medicine name", None
+
+    drug_list, med_drugs_dict = get_drug_name_from_med(med_names, print_output)
+    med_drugs_dict = drug_dict_processing(med_drugs_dict)
+
+    if type(drug_list) == list:
+        drug_list_final = final_drug_list(drug_list)
+        print('Main.py, Drug List: ', drug_list_final)
+        interaction_dict_list = get_interactions_from_drug(drug_list_final, print_output)
+        return interaction_dict_list, med_drugs_dict
+
+    print('Main.py, Drug List: ', drug_list)
+    return drug_list, None
 
 
 def get_drug_name_from_med(med_names: str, print_output: bool = False):
@@ -19,28 +30,23 @@ def get_drug_name_from_med(med_names: str, print_output: bool = False):
         url = 'https://www.mims.com/india/drug/info/' + med_name
         request_result = requests.get(url)
         soup = bs4.BeautifulSoup(request_result.text, "html.parser")
-
         try:
             medicine_raw_text = soup.find("div", "monograph-section-content").text
-
             # Removing white spaces and tabs
             medicine_raw_text = re.sub("[\n\r]", "", medicine_raw_text)
-
             # Making all text lowercase
             medicine_raw_text = medicine_raw_text.lower()
-
             # Resolving text issues for cases with ":"
             if ":" in medicine_raw_text:
                 drug_list = clean_text(medicine_raw_text)
             else:
                 drug_list = clean_text_without_colon(medicine_raw_text)
-
             all_drug_names_dict[med_name] = drug_list
         except Exception as e:
-            print(e)
             print("No drug found for {}".format(med_name))
-
-
+            all_drug_names_dict[med_name] = ["Not in database"]
+    if not bool(all_drug_names_dict):
+        return "No drugs found in database"
     for med_name, drug_list in all_drug_names_dict.items():
         clean_drug_list = remove_common_drugs(drug_list)
         all_drug_names_dict[med_name] = clean_drug_list
@@ -49,38 +55,53 @@ def get_drug_name_from_med(med_names: str, print_output: bool = False):
         for key, value in all_drug_names_dict.items():
             print("{}    : {}".format(key, value))
 
-    return list(itertools.chain(*list(all_drug_names_dict.values())))
+    return list(itertools.chain(*list(all_drug_names_dict.values()))), all_drug_names_dict
 
 
-
-def get_interactions_from_drug(drug_names: dict, print_output: bool = False):
+def get_interactions_from_drug(drug_names: list, print_output: bool = False):
     lst = []
-
+    interaction_dict_list = []
     for drug_name in drug_names:
         if print_output:
             print("trying for {}".format(drug_name))
         url = "https://rxnav.nlm.nih.gov/REST/rxcui.json?name={}&search=1".format(drug_name)
-
         data = requests.get(url).json()
-
-        lst.append(data['idGroup']['rxnormId'][0])
+        try:
+            lst.append(data['idGroup']['rxnormId'][0])
+        except Exception as e:
+            print("Couldn't find data for {}".format(drug_name))
 
     url = 'https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis='
+
     for x in lst:
         url = url + x + '+'
 
-    result = requests.get(url[:-1]).json()
-    for fullInteractionTypeGroup in result['fullInteractionTypeGroup']:
-        for fullInteractionType in fullInteractionTypeGroup['fullInteractionType']:
-            for interactionPair in fullInteractionType['interactionPair']:
-                drug1 = interactionPair['interactionConcept'][0]['sourceConceptItem']['name']
-                drug2 = interactionPair['interactionConcept'][1]['sourceConceptItem']['name']
-                print("Interaction between {} and {}".format(drug1, drug2))
-                print("Severity: {} ".format(interactionPair['severity']))
-                print("Interaction: {}".format(interactionPair['description']))
-                print("-------")
+    try:
+        result = requests.get(url[:-1]).json()
+        for fullInteractionTypeGroup in result['fullInteractionTypeGroup']:
+            for fullInteractionType in fullInteractionTypeGroup['fullInteractionType']:
+                for interactionPair in fullInteractionType['interactionPair']:
+                    drug1 = interactionPair['interactionConcept'][0]['sourceConceptItem']['name']
+                    drug2 = interactionPair['interactionConcept'][1]['sourceConceptItem']['name']
+
+                    interactions_dict = {
+                        'Drug1': drug1,
+                        'Drug2': drug2,
+                        'Severity': interactionPair['severity'],
+                        'Desc': interactionPair['description']
+                    }
+
+                    interaction_dict_list.append(interactions_dict)
+
+                    print("Interaction between {} and {}".format(drug1, drug2))
+                    print("Severity: {} ".format(interactionPair['severity']))
+                    print("Interaction: {}".format(interactionPair['description']))
+                    print("-------")
+        return interaction_dict_list
+    except Exception as e:
+        return "No interactions found in database"
 
 
 if __name__ == "__main__":
-    raw_text = "dolo, volini gel, allegra"
+    raw_text = "dolo, CCM"
     get_interactions_from_med(raw_text, print_output=True)
--- utils.py
+++ utils.py
@@ -1,36 +1,34 @@
 import re
 
+
 def clean_text(text):
     lst = []
     pattern = "[\d\.\s]+(mg[//]?|ml)+"
-    clean_string = re.sub(pattern, ' ', text )
-    
+    clean_string = re.sub(pattern, ' ', text)
+
     pattern = "\([a-z]+\)"
-    clean_string = re.sub(pattern, ' ', clean_string )
+    clean_string = re.sub(pattern, ' ', clean_string)
 
     pattern = ":\s*[a-z\,\-\s]+"
-    final_string = re.findall(pattern, clean_string )
-    
-    
+    final_string = re.findall(pattern, clean_string)
+
     for drug in final_string:
         lst.append(drug.split(":")[1])
-        
+
     return lst
 
 
 def clean_text_without_colon(text):
-    
-#     pattern = "[\r\n]"
-#     clean_string = re.sub(pattern, '', text )
-    
+    #     pattern = "[\r\n]"
+    #     clean_string = re.sub(pattern, '', text )
+
     pattern = "[a-z,\s]+"
     clean_string = re.findall(pattern, text)
-    
+
     # Assuming all text in the first string only
     lst = []
     lst.append(clean_string[0])
     return lst
-
 
 
 def remove_common_drugs(drugs_list):
@@ -63,3 +61,27 @@ def correct_dict(drug_dict):
                         lst.append(sub_item)
                     drug_dict[key] = lst
     return drug_dict
+
+
+def final_drug_list(drug_list):
+    final_drugs = []
+    for drug in drug_list:
+        names = drug.split(',')
+        for name in names:
+            final_drugs.append(name.strip())
+
+    return final_drugs
+
+
+def drug_dict_processing(med_drug_dict):
+    for med, druglist in med_drug_dict.items():
+        org_list = druglist
+        final_drugs = []
+
+        for drug in org_list:
+            names = drug.split(',')
+            for name in names:
+                final_drugs.append(name.strip())
+
+        med_drug_dict[med] = final_drugs
+    return med_drug_dict
